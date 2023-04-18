14d79e360cfa1e2acb3754065d9e8205d7a42b1b
Rozuliony <129408092+Rozuliony@users.noreply.github.com>
2023-04-18 16:26:59 +0200

    Update Mail.js

--- MailPage/Mail.js
+++ MailPage/Mail.js
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const fs = require('fs');
 const login = window.document.getElementById('login');
 const password = window.document.getElementById('password');
