f463c14ad8b5756459ed4b115d58da886c2584e1
Rozuliony <129408092+Rozuliony@users.noreply.github.com>
2023-04-19 09:55:22 +0200

    Update Mail.js

--- MailPage/Mail.js
+++ MailPage/Mail.js
@@ -1,7 +1,7 @@
 const login = window.document.getElementById('login');
 const password = window.document.getElementById('password');
 
-function Send(to, subject, body) {
+
   Email.send({
     SecureToken : "5674e0b3-9ba8-4b97-b292-40736d2a45b8",
     To : to,
@@ -11,4 +11,3 @@ function Send(to, subject, body) {
 }).then(
   message => alert(message)
 );
-}
